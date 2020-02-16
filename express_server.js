2fc3c5e25bddb145b253bed99966f5200ba45231
georgesavill <gpsavill+github@gmail.com>
2020-02-16 16:59:05 +0000

    Remove HTTPS support
    Removed HTTPS support, as this will be handled by the reverse proxy.

--- express_server.js
+++ express_server.js
@@ -1,11 +1,8 @@
 #!/usr/bin/env node /** express_server.js */
 const express = require("express");
 const app = express();
-const app_redirect = express();
 const RateLimit = require("express-rate-limit");
-const fs = require("fs");
 const helmet = require("helmet");
-const https = require("https");
 const http = require("http");
 const path = require("path");
 
@@ -14,21 +11,12 @@ const limiter = new RateLimit({windowMs: 10*60*1000,max:100,delayMs:0});
 app.use(limiter);
 app.use(helmet());
 
-app.use(express.static(path.join(__dirname,"/../georgesavill.com/www/")));
+app.use(express.static(path.join(__dirname,"/../portfolio-website/www/")));
 
 app.get("/", (req, res) => res.send(
     res.sendFile(__dirname, "/index.html")
 ));
-app_redirect.all("/*", (req, res) => res.redirect("https://" + req.headers.host + req.url));
 
-https.createServer({
-    key: fs.readFileSync("/etc/letsencrypt/live/georgesavill.com/privkey.pem"),
-    cert: fs.readFileSync("/etc/letsencrypt/live/georgesavill.com/cert.pem"),
-    ca: fs.readFileSync("/etc/letsencrypt/live/georgesavill.com/chain.pem"),
-}, app).listen(8443,() => {
-    console.log("Server listening on port 443");
-});
-
-http.createServer(app_redirect).listen(8080, () => {
-    console.log("Redirect server listening on port 80");
+http.createServer(app).listen(8081, () => {
+    console.log("Redirect server listening on port 8081");
 });

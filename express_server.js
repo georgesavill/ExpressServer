0a0a348d6630afe0f3eb9856d90c52d7a8e304ba
georgesavill <gpsavill+github@gmail.com>
2020-02-16 18:14:15 +0000

    Remove unneeded semi-colons

--- express_server.js
+++ express_server.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node /** express_server.js */
-const express = require("express");
-const app = express();
-const RateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const http = require("http");
-const path = require("path");
+const express = require("express")
+const app = express()
+const RateLimit = require("express-rate-limit")
+const helmet = require("helmet")
+const http = require("http")
+const path = require("path")
 
-const limiter = new RateLimit({windowMs: 10*60*1000,max:100,delayMs:0});
+const limiter = new RateLimit({windowMs: 10*60*1000,max:100,delayMs:0})
 
 app.use(limiter);
 app.use(helmet());
 
-app.use(express.static(path.join("/www/")));
+app.use(express.static(path.join("/www/")))
 
 app.get("/", (req, res) => res.send(
     res.sendFile("/www/index.html")
 ));
 
 http.createServer(app).listen(8081, () => {
-    console.log("Redirect server listening on port 8081");
+    console.log("Redirect server listening on port 8081")
 });
